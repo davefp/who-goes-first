bc727f24a9c24d5468eae87d66f8137deb3df573
David Underwood <davefp@gmail.com>
2012-04-09 20:03:46 -0400

    adds ajax reload for new rule

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,16 +1,20 @@
 class RulesController < ApplicationController
-   def index
-     redirect_to :root
-   end
-   
-   def random
-     @rule = Rule.with_state(:approved).random
-	 @new_rule = Rule.new()
-   end
-   
-   def create
-     Rule.create(params[:rule])
-	 flash[:notice] = "Thanks! Your rule has been submitted for moderation."
-	 redirect_to :root
-   end
+  def index
+    redirect_to :root
+  end
+
+  def random
+    @rule = Rule.with_state(:approved).random
+    @new_rule = Rule.new()
+    respond_to do |format|
+      format.html { render }
+      format.js   { render }
+    end
+  end
+
+  def create
+    Rule.create(params[:rule])
+    flash[:notice] = "Thanks! Your rule has been submitted for moderation."
+    redirect_to :root
+  end
 end
