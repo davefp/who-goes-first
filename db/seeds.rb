8c021393170eaaa00f20ab033d6fac8501edbf5f
David Underwood <david.underwood@jadedpixel.com>
2013-01-26 16:45:34 -0500

    Adds db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Rule.destroy_all
+
+Rule.create(:rule_text => "The person with the longest hair goes first", :state => "approved")
