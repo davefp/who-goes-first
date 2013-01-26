51d54c831d4d6750e061fd620168d6627ed87c97
David Underwood <david.underwood@jadedpixel.com>
2013-01-26 16:59:20 -0500

    Fixes show rule handler

--- app/controllers/rules_controller.rb
+++ app/controllers/rules_controller.rb
@@ -1,11 +1,12 @@
 class RulesController < ApplicationController
+  before_filter :template_rule
+
   def index
     redirect_to :root
   end
 
   def random
     @rule = Rule.with_state(:approved).random
-    @new_rule = Rule.new()
     render :show
   end
 
@@ -18,5 +19,11 @@ class RulesController < ApplicationController
     Rule.create(params[:rule])
     flash[:notice] = "Thanks! Your rule has been submitted for moderation."
     redirect_to :root
+  end
+
+  private
+
+  def template_rule
+    @new_rule = Rule.new
   end
 end
